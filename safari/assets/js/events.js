7a35a7aacd4f52d541602677f58e82d4f412abb0
Adem ilter <ailter@protel.com.tr>
2016-06-08 14:05:02 +0300

    change base

--- safari/assets/js/events.js
+++
@@ -1,7 +0,0 @@
-if (!safari.extension.settings.hasRun) {
- 	safari.extension.settings.hasRun = true;
-	safari.extension.settings.lang = window.navigator.language;
-	openURLInNewTab(salyangozWelcomeURL);
-}
-
-safari.application.addEventListener("command", performCommand, false);
